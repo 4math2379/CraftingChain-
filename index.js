8d355a7b0dbd88478dfbdd61dfcf26214dee7a59
Rahma <johnniy5@me.com>
2018-01-23 07:59:45 +0100

    MAJ

--- index.js
+++ index.js
@@ -11,12 +11,22 @@ var git         = require('simple-git')();
 var touch       = require('touch');
 var fs          = require('fs');
 var files = require('./lib/lib');
+var programm = require('commander');
+var colors = require('colors');
+var inquirer = require('inquirer');
 
 
+
+
+
+
+
+
+//build
 //intro
 clear();
 console.log(
-    chalk.cyan(
+    chalk.red(
 	figlet.textSync('A n v i l', {
 	    horyzontalLayout: 'full'
 	
@@ -30,10 +40,92 @@ console.log(
 	
 	
 
-//git check
-if (files.directoryExists('.git')) {
-  console.log(chalk.red('Already a git repository!'));
-  process.exit();
-}
 
-	
+
+
+//commander
+
+programm
+    .version('0.1.1')
+    .option('-p, --push', 'pushed')
+    .option('-o, --open', 'opened')
+    .option('-help, --help', 'help')
+    .option('-l, --login', 'login')
+    .parse(process.argv);
+
+console.log('command usedh:');
+if (programm.push) console.log('  - pushed'.green);
+if (programm.open) console.log('opened'.green);
+
+//log interface
+
+
+
+
+
+
+//question lol
+var questions = [
+{
+    type:'input',
+    name:'email',
+    message:'Please enter your Email',
+    default: false
+    },
+{
+    type:'list',
+    name:'activities',
+    message:'What is your role ?',
+    choices: ['Hobbyist','Developer','Designer'],
+    filter: function (val) {
+    
+    
+	return val.toLowerCase();
+    
+    
+    }
+    
+
+
+
+
+
+
+
+}];
+
+
+if (programm.login) inquirer.prompt (questions).then (function (answers) {
+
+
+
+
+
+
+
+    console.log('\nYour information:');
+    console.log(JSON.stringify(answers, null, ' '));
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
