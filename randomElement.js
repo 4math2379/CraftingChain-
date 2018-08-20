b28cde2e0a51eed2e8864831f7e6837911b9f757
Sinien Rahma <johnniy5@me.com>
2018-08-20 15:22:10 +0200

    add ressources and testing NEO
    add ressource.
    use Neo Blockchain for a test ( gas in waiting)

---
+++ randomElement.js
@@ -0,0 +1,28 @@
+//put random element code for CRAKING events
+
+
+//when player decide not to sell his ressources 'chunk' he harvest he can "Crack" it , more the ressource got better stats
+//more the ressource can be rare.
+
+
+
+
+ function crackEvent() {
+     var pickedRessources = Math.floor((Math.random()* 3) + 1);
+     console.log(pickedRessources);
+
+     if (pickedRessources == 1) {
+         console.log("you craked Adamantium , stats is " + adamantium)
+         
+     } else if (pickedRessources == 2) {
+        console.log("you craked Silver , stats is " + silver)
+
+     } else {
+        console.log("you craked Bronze , stats is " + bronze)
+     }
+    
+
+ }
+ crackEvent();
+
+
