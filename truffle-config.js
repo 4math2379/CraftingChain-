22301c4c34e9bcdcde01c218d74d2a7ddb59071d
Rahma <rahma3.1sita@gmail.com>
2019-06-06 11:41:20 +0200

    add resource and build Wen3 popup

--- truffle-config.js
+++ truffle-config.js
@@ -11,20 +11,29 @@
  *     gasPrice: 10000000000,
  *   },
  */
-var HDWalletProvider = require("truffle-hdwallet-provider");
+//var HDWalletProvider = require("truffle-hdwallet-provider");
 
-var mnemonic = "parent crazy slot apple payment decline zebra demise stock census slow thrive"
+//var mnemonic = "parent crazy slot apple payment decline zebra demise stock census slow thrive"
 
 module.exports = {
-  networks: {
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/583c67d6aae34e3794a8b5fd9009dc93")
-      },
-      network_id: '3',
-      gas: 4000000,
-      gasPrice: 21
-    }   
+
+
+  development: {
+    host: "127.0.0.1",
+    port: 7545,
+    network_id: "*" // Match any network id
   }
+
+
+  // networks: {
+  //   ropsten: {
+  //     provider: function() {
+  //       return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/583c67d6aae34e3794a8b5fd9009dc93")
+  //     },
+  //     network_id: '3',
+  //     gas: 4000000,
+  //     gasPrice: 21
+  //   }   
+  // }
 
 };
