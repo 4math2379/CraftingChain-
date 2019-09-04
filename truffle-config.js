10e2354caa58e1afc03ac290d3bb0d3dd7698ed0
Sinien Rahma <johnniy5@me.com>
2019-09-04 16:52:19 +0200

    update node modules

--- truffle-config.js
+++ truffle-config.js
@@ -5,16 +5,20 @@ var mnemonic = "parent crazy slot apple payment decline zebra demise stock censu
 
 module.exports = {
 
+  plugins: ["truffle-security"],
+
+
 
   
 
 
    networks: {
-    /*development: {
+    plugins: [ "truffle-security" ],
+    development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    },*/
+    },
      ropsten: {
        provider: function() {
          return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/583c67d6aae34e3794a8b5fd9009dc93")
