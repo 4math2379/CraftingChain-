011d24efa33d9df63d71958b5c1743a4f6c6d0b1
Sinien Rahma <4math2379@users.noreply.github.com>
2020-04-01 14:31:29 +0200

    none

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
   ropsten: {
-    provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/583c67d6aae34e3794a8b5fd9009dc93" ),
+    provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY ),
       network_id: '3',
       host:"localhost",
       port:3000,
